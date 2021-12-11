d57cb8d698f3d8c5b3956c71a619e36b52c60578
Salvatore P.C <salvatore.pannozzocapodiferro@gmail.com>
2021-12-11 20:49:15 +0000

    ex 16

--- exercises/16-Random-Colors-Loop/app.py
+++ exercises/16-Random-Colors-Loop/app.py
@@ -19,9 +19,12 @@ def get_allStudentColors():
     example_color = 1
     students_array = []
     #your loop here
+    for x in range(1,11):
+        r = random.randint(0,4)
+        color = get_color(r)
 
-
-
-
+        students_array.append(color)
+     #   print(x,r,color)
+    return students_array
 
 print(get_allStudentColors())
