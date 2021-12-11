faf9f3a550e601813a8e7902500701a22d2fe383
Salvatore P.C <salvatore.pannozzocapodiferro@gmail.com>
2021-12-11 21:05:37 +0000

    ex17

--- exercises/17-Russian-Roulette/app.py
+++ exercises/17-Russian-Roulette/app.py
@@ -1,6 +1,6 @@
 import random
 
-bullet_position = 3
+bullet_position = 1
 
 def spin_chamber():
 	chamber_position = random.randint(1,6)
@@ -9,8 +9,14 @@ def spin_chamber():
 #  DON'T CHANGE THE CODE ABOVE
 def fire_gun():
 	# YOUR CODE HERE
+	global bullet_position
+	x = spin_chamber()
 
-
+	if x == bullet_position:
+		return 'You are dead!'
+	else:
+		bullet_position=bullet_position+1
+		return 'Keep playing!'
 
 
 print(fire_gun())
