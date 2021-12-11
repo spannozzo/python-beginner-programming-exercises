560500fb67989a8459aeed52b9fc8accaf821d4d
Salvatore P.C <salvatore.pannozzocapodiferro@gmail.com>
2021-12-11 02:29:49 +0000

    ex 9

--- exercises/09-Random-Numbers/app.py
+++ exercises/09-Random-Numbers/app.py
@@ -2,9 +2,9 @@ import random
 
 def get_randomInt():
   # CHANGE ONLY THIS LINE BELOW
-	random_number = random.random()
+	random_number = random.randrangegi(1,10)
 	return random_number
-
+	
 
 
 print(get_randomInt())
